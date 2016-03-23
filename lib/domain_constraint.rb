4f83a2ef661c859318d4870bae0434c06581aaf8
Vadim Dribinsky <vadimd@autobytel.com>
2016-03-23 14:34:38 -0400

    fixing domains

--- lib/domain_constraint.rb
+++ lib/domain_constraint.rb
@@ -1,7 +1,7 @@
 class DomainConstraint
   def initialize
     db_config = Rails.application.config.database_configuration[Rails.env]
-    client = Mysql2::Client.new(:host => db_config['host'], :database => Rails.env, :username => db_config['username'], :password => db_config['password'])
+    client = Mysql2::Client.new(:host => db_config['host'], :database => db_config['database'], :username => db_config['username'], :password => db_config['password'])
     @domains = client.query("select name, base_url from clients where name != 'welcome'")
   end
 
